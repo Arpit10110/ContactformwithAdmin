687c26b1fa3159ed62153cc3a28001be50262bf9
Arpit <omagrahari55@gmail.com>
2023-10-12 22:49:12 +0530

    second  commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {BrowserRouter as Router, Routes ,Route} from "react-router-dom"
+import {HashRouter as Router, Routes ,Route} from "react-router-dom"
 import ContactForm from "./Components/ContactForm.jsx"
 import Navbar from "./Components/Navbar.jsx"
 import Admin from "./Components/Admin.jsx"
